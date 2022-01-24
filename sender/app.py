f618b5a770109497a0a3f7d5e15c4bbb6cb4b529
Brollof <thefish3r@gmail.com>
2022-01-24 21:41:58 +0100

    resolving relative config path

--- sender/app.py
+++ sender/app.py
@@ -1,19 +1,26 @@
 import socket
 import configparser
 import time
-
+import os
 try:
     import w1thermsensor
 except:
     import mock.w1thermsensor as w1thermsensor
 
+
+def get_fullpath(filename):
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    return os.path.join(script_dir, filename)
+
+
 TEMP_CHECK_PERIOD = 1  # seconds
+CONFIG_FILE_PATH = get_fullpath('config.ini')
 
 print("Sinux sender app started")
 
 sensor = w1thermsensor.W1ThermSensor()
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read(CONFIG_FILE_PATH)
 
 host = config['DEFAULT']['Host']
 port = int(config['DEFAULT']['Port'])
